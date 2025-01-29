7e98cfcd454fa2dc049b9e7ac8d06bc2c7f4b30f
Franek313 <72795920+Franek313@users.noreply.github.com>
2025-01-29 13:53:44 +0100

    Init Commit

---
+++ GeneratorEtykiet/ExportWindow.xaml.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using ZXing.OneD;
+
+namespace GeneratorEtykiet
+{
+    /// <summary>
+    /// Interaction logic for ExportWindow.xaml
+    /// </summary>
+    public partial class ExportWindow : Window
+    {
+        public ObservableCollection<YourDataRow> Rows { get; set; }
+        public dynamic data;
+        public int currentRowIndex = 0;
+
+        public ExportWindow()
+        {
+            InitializeComponent();
+            Rows = new ObservableCollection<YourDataRow>();
+            dataGrid.ItemsSource = Rows;
+            ReadJSONDatabase();
+        }
+
+        private void ReadJSONDatabase()
+        {
+            string json = File.ReadAllText("data.json");
+            data = JsonConvert.DeserializeObject(json);
+        }
+
+        private void DodajWiersz_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> productNames = new List<string>();
+            for(int i = 0; i < data.Count; i++)
+            {
+                productNames.Add(data[i]["Opis_PL"].ToString());
+            }
+            Rows.Add(new YourDataRow(productNames, 0));
+        }
+
+        // Klasa reprezentująca dane w wierszu
+        public class YourDataRow
+        {
+            public int ID { get; set; }
+            public ObservableCollection<string> ListaElementow { get; set; }
+            public string WybranyElement { get; set; }
+            public ObservableCollection<string> Sloje { get; set; }
+            public string WybranySloj { get; set; }
+            public string Ilosc { get; set; }
+
+            public YourDataRow(List<string> opcje, int id)
+            {
+                ListaElementow = new ObservableCollection<string>(opcje);
+                Sloje = new ObservableCollection<string>(new List<string> { "PION", "POZ" });
+                WybranyElement = ListaElementow[0];
+                WybranySloj = Sloje[0];
+                ID = id;
+                Ilosc = "1";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        int idEtykiety = 0;
+        private void GeneratePNG(int id, int sloj)
+        {
+            LOGS.Text += "PNG GENERATION\n";
+            MainWindow mainWindow = new MainWindow(id);
+            DateTime now = DateTime.Now;
+            string formattedDate = now.ToString("dd.MM.yyyy");
+            mainWindow.Kod.Text = $"{Rows[sloj].WybranySloj} {formattedDate} {idEtykiety}";
+            idEtykiety++;
+            mainWindow.Show(); 
+            mainWindow.ExportToPng(mainWindow.imageGrid, "Images/Temp/temp.png");
+            mainWindow.Close();
+        }
+
+        public void GeneratePDF(List<Tuple<int,int>> elements)
+        {
+            var document = new PdfDocument();
+            
+            try
+            {
+                var page = new PdfPage();
+
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    if (i != 0)
+                    {
+                        if (elements[i] != elements[i - 1])
+                        {
+                            GeneratePNG(elements[i].Item1, elements[i].Item2);
+                        }
+                    }
+                    else
+                    {
+                        GeneratePNG(elements[i].Item1, elements[i].Item2);
+                    }
+                    
+                    int j = i%4;
+                    
+                    if (i % 4 == 0)
+                    {
+                        page = document.AddPage();
+                        page.Size = PdfSharpCore.PageSize.A4;
+                    }
+
+                    // Utwórz obiekt XGraphics do rysowania na stronie
+                    using (var gfx = XGraphics.FromPdfPage(page))
+                    {
+                        // Wczytaj obraz z pliku
+                        string imagePath = "Images/Temp/temp.png"; // Ścieżka do obrazu
+                        XImage image = XImage.FromFile(imagePath);
+
+                        double width = page.Width; // Szerokość obrazu
+                        double height = image.PixelHeight * page.Width / image.PixelWidth; // Wysokość obrazu
+
+                        // Określ pozycję i rozmiar obrazu na stronie
+                        double x = 0; // Pozycja X
+                        double y = j * height; // Pozycja Y
+
+                        // Narysuj obraz na stronie PDF
+                        gfx.DrawImage(image, x, y, width, height);
+                    }
+                }
+                    
+                
+                // Inicjalizuj obiekt SaveFileDialog do zapisu pliku
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    // Uzyskaj wybraną ścieżkę do zapisu
+                    string filePath = saveFileDialog.FileName;
+
+                    // Zapisz dokument PDF do pliku
+                    document.Save(filePath);
+
+                    // Otwórz plik PDF w domyślnej aplikacji do przeglądania plików PDF
+                    System.Diagnostics.Process.Start(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Obsłuż wyjątek związany z generowaniem pliku PDF
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Zamknij dokument PDF
+                document.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<Tuple<int, int>> pairedElements = new List<Tuple<int, int>>();
+            //List<int> elements = new List<int>();
+
+            LOGS.Text = "";
+
+            string pattern = @"^(?=.*\d)[\d]+$";
+
+            for(int i = 0; i < Rows.Count; i++)
+            {
+                bool isValid = Regex.IsMatch(Rows[i].Ilosc, pattern);
+                if (!isValid)
+                {
+                    MessageBox.Show("Podano nieprawidłową wartość pola Ilość!", "UWAGA!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                if (Rows[i].WybranyElement == null)
+                {
+                    MessageBox.Show("Nie podano nazwy etykiety!", "UWAGA!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                for (int j = 0; j < int.Parse(Rows[i].Ilosc); j++)
+                    pairedElements.Add(new Tuple<int, int>(Rows[i].ID, i));
+            }
+
+            if(Rows.Count != 0)
+                GeneratePDF(pairedElements);
+
+            LOGS.Text += "START PDF GENERATION\n";
+            
+        }
+
+        private void DataGrid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                // Sprawdź, czy zaznaczony jest jakiś wiersz
+                if (dataGrid.SelectedItem != null)
+                {
+                    // Usuń zaznaczony wiersz
+                    Rows.Remove(dataGrid.SelectedItem as YourDataRow);
+                }
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+
+            YourDataRow selectedRow = comboBox.DataContext as YourDataRow;
+
+            selectedRow.WybranyElement = comboBox.SelectedItem as string;
+            selectedRow.ID = comboBox.SelectedIndex;
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            YourDataRow selectedRow = comboBox.DataContext as YourDataRow;
+
+            selectedRow.WybranySloj = comboBox.SelectedItem as string;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                Rows.Remove(dataGrid.SelectedItem as YourDataRow);
+            }
+        }
+
+        
+    }
+}
---
+++ GeneratorEtykiet/MainWindow.xaml.cs
@@ -0,0 +1,229 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using ZXing;
+using System.Drawing;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Controls.Primitives;
+
+namespace GeneratorEtykiet
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int ID = -1;
+        
+        public MainWindow(int id)
+        {
+            ID = id;
+            InitializeComponent();
+            LoadFieldsFromJSON(ID);
+        }
+
+        public MainWindow()
+        {
+            LoadFieldsFromJSON(ID);
+        }
+
+        public void ExportToPng(FrameworkElement element, string filePath)
+        {
+            
+            // Tworzenie RenderTargetBitmap z określonego elementu
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap((int)element.ActualWidth, (int)element.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            renderBitmap.Render(element);
+            CMD.Text += "BP";
+            // Konwertowanie do formatu PNG
+            PngBitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+
+            // Zapisanie do pliku
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                pngEncoder.Save(fileStream);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Tworzenie okna dialogowego do wyboru lokalizacji i nazwy pliku
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pliki PNG (*.png)|*.png";
+            saveFileDialog.Title = "Zapisz obrazek jako...";
+
+            // Wyświetlenie okna dialogowego
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                // Jeśli użytkownik wybrał lokalizację i nazwę pliku, eksportuj obrazek
+                ExportToPng(imageGrid, saveFileDialog.FileName);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ExportWindow exportWindow = new ExportWindow();
+            exportWindow.Show();
+        }
+
+        public void LoadFieldsFromJSON(int productID)
+        {
+            string json = File.ReadAllText("data.json");
+
+            dynamic dane = JsonConvert.DeserializeObject(json);
+
+            PrzypiszDaneDoTextBox("Nazwa", dane[productID]);
+                
+            string uwagi = dane[productID]["Uwagi"];
+            var sections = uwagi.Split(' ');
+            var dimensions = sections[0].Split('x');
+            var box_dimensions = sections[1].Split('x');
+            var weight = sections[2];
+
+            PrzypiszDaneDoTextBox("Opis_PL", dane[productID]);
+            PrzypiszDaneDoTextBox("Dimensions", $"wysokość {dimensions[1]}cm, szerokość {dimensions[0]}cm, głębokość {dimensions[2]}cm");
+            PrzypiszDaneDoTextBox("Box_X_Dimension", $"{box_dimensions[0]}cm");
+            PrzypiszDaneDoTextBox("Box_Y_Dimension", $"{box_dimensions[1]}cm");
+            PrzypiszDaneDoTextBox("Box_Z_Dimension", $"{box_dimensions[2]}cm");
+            PrzypiszDaneDoTextBox("Weight", $"{weight}kg");
+            PrzypiszDaneDoTextBox("Symbol", dane[productID]);
+            AssignBarcodeToImage("Kod_kreskowy", dane[productID]);
+        }
+
+        public void LoadFieldsFromJSONAndUpdateTempImage(int productID)
+        {
+            LoadFieldsFromJSON(productID);
+            ExportToPng(imageGrid, "Images/Temp/temp.png");
+        }
+
+        private void PrzypiszDaneDoTextBox(string nazwaTextBox, dynamic dane)
+        {
+            if (dane[nazwaTextBox] != null)
+            {
+                var textBox = FindName(nazwaTextBox) as TextBox;
+
+                if (textBox != null)
+                {
+                    textBox.Text = dane[nazwaTextBox].ToString();
+                }
+                else
+                {
+                    MessageBox.Show($"Nie znaleziono TextBoxa o nazwie {nazwaTextBox}.");
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Nie znaleziono danych JSON dla klucza {nazwaTextBox}.");
+            }
+        }
+
+        private void PrzypiszDaneDoTextBox(string nazwaTextBox, string text)
+        {
+            var tb = FindName(nazwaTextBox) as TextBox;
+            tb.Text = text;
+        }
+
+        public Bitmap GenerateBarcode(long number)
+        {
+            BarcodeWriter writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.EAN_13,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Height = 234,
+                    Width = 400,
+                    PureBarcode = true
+                }
+            };
+
+            // Konwersja numeru całkowitego na ciąg znaków
+            string barcodeText = number.ToString().PadLeft(13, '0'); // Dodanie zer wiodących, aby uzyskać 13 cyfr dla formatu EAN-13
+
+            // Generowanie kodu kreskowego na podstawie ciągu znaków
+            Bitmap barcodeBitmap = writer.Write(barcodeText);
+
+            return barcodeBitmap;
+        }
+
+        // Funkcja przypisująca wygenerowany kod kreskowy do pola Image w aplikacji WPF
+        public void AssignBarcodeToImage(string keyValue, dynamic dane)
+        {
+            var number = dane[keyValue].ToString();
+            // Wygenerowanie kodu kreskowego
+            Bitmap barcodeBitmap = GenerateBarcode(long.Parse(number));
+            var textBox = FindName("Kod_kreskowy") as TextBox;
+            textBox.Text = number;
+
+            // Konwersja kodu kreskowego (Bitmap) na obrazek BitmapImage
+            BitmapImage bitmapImage = new BitmapImage();
+            using (var memory = new System.IO.MemoryStream())
+            {
+                barcodeBitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memory;
+                bitmapImage.EndInit();
+            }
+
+            // Przypisanie obrazka do pola Image w aplikacji WPF
+            var barcodeImage = (System.Windows.Controls.Image)FindName("BarCode");
+            barcodeImage.Source = bitmapImage;
+        }
+
+        public void AssignBarcodeToImageFromInputField(long value)
+        {
+            // Wygenerowanie kodu kreskowego
+            Bitmap barcodeBitmap = GenerateBarcode(value);
+
+            // Konwersja kodu kreskowego (Bitmap) na obrazek BitmapImage
+            BitmapImage bitmapImage = new BitmapImage();
+            using (var memory = new System.IO.MemoryStream())
+            {
+                barcodeBitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memory;
+                bitmapImage.EndInit();
+            }
+
+            // Przypisanie obrazka do pola Image w aplikacji WPF
+            var barcodeImage = (System.Windows.Controls.Image)FindName("BarCode");
+            barcodeImage.Source = bitmapImage;
+        }
+
+        private void BarCodeText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            long value = 0;
+
+            if (!long.TryParse(textBox.Text, out value))
+            {
+                return;
+            }
+
+            AssignBarcodeToImageFromInputField(value);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
